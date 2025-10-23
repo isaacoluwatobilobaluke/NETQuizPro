1d36785b6b28e8e59da696dde9855cec22c8522d
isaacoluwatobilobaluke <oluwatobilobaisaac675@gmail.com>
2025-10-23 12:09:20 +0100

    feat: add backend server.js starter

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Mongo Atlas connected'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
